d177bcc4578eaee2f735c57b03cc518e902c2700
dmc4719 <dmc4719@gmail.com>
2020-05-11 23:58:43 +0600

    fixed the seekbar issue

--- app/src/main/java/com/example/redhawkmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/redhawkmusicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -53,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar seekBar;
     static MediaPlayer myMediaPlayer;
     int pos;
-    Thread updateSeekBar;
-
-
+//    Thread updateSeekBar;
+    private Runnable runnable;
+    private Handler handler;
     GlobalFunctions uiBars = new GlobalFunctions();
     private View decorView;
     @Override
@@ -72,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         cur_time = findViewById(R.id.cur_time);
         full_time = findViewById(R.id.full_time);
 
-
+        handler = new Handler();
 
 
         songtitle = findViewById(R.id.song_name);
@@ -133,39 +134,44 @@ public class MainActivity extends AppCompatActivity {
                   }
               }).check();
 
-
-
-
-      updateSeekBar = new Thread(){
-        @Override
-        public void run(){
-            int totalDuration = myMediaPlayer.getDuration();
-            int currentPosition = 0;
-
-            while (currentPosition<totalDuration){
-                try{
-                    sleep(500);
-                    currentPosition = myMediaPlayer.getCurrentPosition();
-                    seekBar.setProgress(currentPosition);
-                    Log.i("position",""+currentPosition);
-                    cur_time.setText(Integer.toString(myMediaPlayer.getCurrentPosition()/1000));
-                }
-                catch(InterruptedException e){
-                    Log.i("error","updateSeekBar issue");
-                    e.printStackTrace();
-                }
-            }
-
-        }
-      };
-
       if(myMediaPlayer!=null){
           myMediaPlayer.stop();
           myMediaPlayer.release();
       }
 
-
     }
+
+
+    private void updateSeekBar(){
+        seekBar.setProgress((myMediaPlayer.getCurrentPosition()));
+
+        Long time = Long.valueOf(myMediaPlayer.getCurrentPosition());
+        long seconds = time/1000;
+        long minutes = seconds/60;
+        seconds = seconds % 60;
+
+        if(seconds<10) {
+            String current_time = String.valueOf(minutes) + ":0" + String.valueOf(seconds);
+            cur_time.setText(current_time);
+        } else {
+            String current_time = String.valueOf(minutes) + ":" + String.valueOf(seconds);
+            cur_time.setText(current_time);
+        }
+
+        if(myMediaPlayer.isPlaying()){
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("seekbar", "updating");
+                    updateSeekBar();
+                }
+            };
+            handler.postDelayed(runnable,1000);
+        }
+    }
+
+
+
   public ArrayList<File> findSong(File file){
         ArrayList<File> arrayList = new ArrayList<>();
 
@@ -209,55 +215,59 @@ public class MainActivity extends AppCompatActivity {
 
       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
           @Override
-          public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+          public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
               pos = position;
               ItemHelper lists = list.get(position);
 
               Uri u = Uri.parse(mySongs.get(position).toString());
-
               if(myMediaPlayer!=null){
                   myMediaPlayer.stop();
                   myMediaPlayer.release();
-
               }
               seekBar.setProgress(0);
-              cur_time.setText("0");
+              cur_time.setText("0:00");
 
               myMediaPlayer = MediaPlayer.create(getApplicationContext(),u);
               myMediaPlayer.start();
+
               songtitle.setText(mySongs.get(position).getName());
               songtitlemini.setText(mySongs.get(position).getName());
-
               play_btn.setBackgroundResource(R.drawable.ic_pause_black_24dp);
               play_btn_mini.setBackgroundResource(R.drawable.ic_pause_black_24dp);
-
-
-              seekBar.setMax(myMediaPlayer.getDuration());
+              int Duration = myMediaPlayer.getDuration()/1000;
               seekBar.setProgress(myMediaPlayer.getCurrentPosition());
-              full_time.setText(Integer.toString(myMediaPlayer.getDuration()/1000));
-
-
-              if(updateSeekBar.getState() == Thread.State.NEW){
-                  Log.i("created"," created");
-                  updateSeekBar.start();
-              }
+              seekBar.setMax(myMediaPlayer.getDuration());
+              full_time.setText(Duration / 60 + ":" + Duration % 60);
 
               myMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                   @Override
                   public void onCompletion(MediaPlayer mp) {
                       seekBar.setProgress(0);
-                      mp.seekTo(0);
-                      Log.i("message","seeked to 0");
-                      cur_time.setText("0");
-
-                      play_btn.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+                      mp.seekTo(seekBar.getProgress());
+                      if(myMediaPlayer.isPlaying()){
+                          Log.i("paused", "here");
+                          myMediaPlayer.pause();
+                          cur_time.setText("0:00");
+                          play_btn.setBackgroundResource(R.mipmap.ic_play);
+                          play_btn_mini.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+                      }
+                      cur_time.setText("0:00");
+                      play_btn.setBackgroundResource(R.mipmap.ic_play);
                       play_btn_mini.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+                      Log.i("restarted", "here");
                   }
               });
+
+              //Updates seekbar while music plays
+                updateSeekBar();
 
               seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                   @Override
                   public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if(fromUser){
+                            myMediaPlayer.seekTo(progress);
+                        }
+
                   }
 
                   @Override
@@ -280,27 +290,15 @@ public class MainActivity extends AppCompatActivity {
                   public void onClick(View v) {
 //                      seekBar.setMax(myMediaPlayer.getDuration());
                         if(myMediaPlayer.isPlaying()){
-                            play_btn.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+                            play_btn.setBackgroundResource(R.mipmap.ic_play);
                             play_btn_mini.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
                             myMediaPlayer.pause();
                         }
-                        else if(seekBar.getProgress() == 0){
-                            Log.i("play_btn", "got In");
-                            play_btn.setBackgroundResource(R.drawable.ic_pause_black_24dp);
-                            play_btn_mini.setBackgroundResource(R.drawable.ic_pause_black_24dp);
-                            myMediaPlayer.start();
-                            if(updateSeekBar.getState() == Thread.State.NEW){
-                                Log.i("created2", "got In");
-                                updateSeekBar.start();
-                            }
-                            else if(updateSeekBar.isAlive()){
-                                Log.i("alive", "Still alive");
-                            }
-                        }
                         else{
-                            play_btn.setBackgroundResource(R.drawable.ic_pause_black_24dp);
+                            play_btn.setBackgroundResource(R.mipmap.ic_pause);
                             play_btn_mini.setBackgroundResource(R.drawable.ic_pause_black_24dp);
                             myMediaPlayer.start();
+                            updateSeekBar();
                         }
                   }
               });
